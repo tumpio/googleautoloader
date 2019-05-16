c9804eaecf73dcb35bf16ff97c95e2084a711b7b
tumpio <tumpio@users.noreply.github.com>
2019-05-16 20:23:55 +0300

    Fetch video thumbs

--- google-auto-loader.js
+++ google-auto-loader.js
@@ -2,6 +2,9 @@ const centerElement = "#center_col";
 const loadWindowSize = 1.6;
 const filtersAll = ["#foot", "#bottomads"];
 const filtersCol = filtersAll.concat(["#extrares", "#imagebox_bigimages"]);
+const twId = "t3y30m6mtdnac5ndl40i7qxmf8f3zt";
+const thumbHeight = "65";
+const thumbWidth = "116";
 
 let pageNumber = 1;
 let prevScrollY = 0;
@@ -33,6 +36,7 @@ function requestNextPage() {
             col.appendChild(pageMarker);
             col.appendChild(content);
             document.querySelector(centerElement).appendChild(col);
+            fetchThumbs(content);
 
             if (!content.querySelector("#ires")) {
                 // end of results
@@ -43,6 +47,63 @@ function requestNextPage() {
             pageNumber++;
             nextPageLoading = false;
         });
+}
+
+function fetchThumbs(document) {
+    let vidThumbs = document.querySelectorAll("[id^='vidthumb']");
+    let twHeaders = {
+        headers: {
+            "Client-ID": twId
+        }
+    };
+    for (let thumb of vidThumbs) {
+        let href = thumb.parentNode.parentNode.href;
+        let yt = href.match(/https:\/\/www\.youtube\.com\/watch\?v=(.+)&?/);
+        if (yt) {
+            thumb.src = `https://img.youtube.com/vi/${yt[1]}/default.jpg`;
+            continue;
+        }
+        let twClip = href.match(/https:\/\/www\.twitch\.tv\/.+\/clip\/(.+)/);
+        if (twClip) {
+            fetch(`https://api.twitch.tv/helix/clips?id=${twClip[1]}&first=1`, twHeaders)
+            .then(response => response.json())
+            .then(result => {
+                thumb.src = result.data[0].thumbnail_url.replace("%{height}", thumbHeight).replace("%{width}", thumbWidth);
+            });
+            continue;
+        }
+        let twVideo = href.match(/https:\/\/www\.twitch\.tv\/videos\/(.+)/);
+        if (twVideo) {
+            fetch(`https://api.twitch.tv/helix/videos?id=${twVideo[1]}`, twHeaders)
+            .then(response => response.json())
+            .then(result => {
+                thumb.src = result.data[0].thumbnail_url.replace("%{height}", thumbHeight).replace("%{width}", thumbWidth);
+            });
+            continue;
+        }
+        let twUser = href.match(/https:\/\/www\.twitch\.tv\/(.+)\/videos$/);
+        if (twUser) {
+            fetch(`https://api.twitch.tv/helix/users?login=${twUser[1]}`, twHeaders)
+            .then(response => response.json())
+            .then(user => {
+                return fetch(`https://api.twitch.tv/helix/videos?user_id=${user.data[0].id}&first=1`, twHeaders);
+            })
+            .then(response => response.json())
+            .then(result => {
+                thumb.src = result.data[0].thumbnail_url.replace("%{height}", thumbHeight).replace("%{width}", thumbWidth);
+            });
+            continue;
+        }
+        let vimeo = href.match(/https:\/\/vimeo\.com\/(.+)/);
+        if (vimeo) {
+            fetch(`https://vimeo.com/api/oembed.json?url=${encodeURIComponent(vimeo[0])}`)
+            .then(response => response.json())
+            .then(result => {
+                thumb.src = result.thumbnail_url;
+            });
+            continue;
+        }
+    }
 }
 
 function onScrollDocumentEnd() {
